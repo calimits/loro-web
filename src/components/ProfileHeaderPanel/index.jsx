262f6073a31a07009852ee283f68b988d656751e
calimits <calimitsacd@gmail.com>
2025-10-25 10:56:55 -0400

    working now on authcontext

--- src/components/ProfileHeaderPanel/index.jsx
+++ src/components/ProfileHeaderPanel/index.jsx
@@ -1,10 +1,13 @@
 import "./Header.css"
+import { useCurrentView } from "../ViewManager/context/currentViewContext"
 
 export default function AccountHeaderPanel() {
+    const {setCurrentView} = useCurrentView();
+
     return (
         <header className="header-bar cool-bar">
             <div className="to-left">
-                <p className="back-btn">←</p>
+                <p className="back-btn" onClick={(e) => setCurrentView("home")}>←</p>
                 <h2 className="app-title">Account</h2>
             </div>
             <input type="text" placeholder="Buscar..." className="search"></input>
