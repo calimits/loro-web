652b8377ec50ee09bfdd0755041139019bf852d0
calimits <calimitsacd@gmail.com>
2025-10-28 17:15:16 -0400

    working on delete contacts and its modularity

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,10 +1,11 @@
 import "./Contact.css";
 import profilePpic from "../../assets/loro-profile.png"
 
-export default function Contact({username}) {
+export default function Contact({username, edit}) {
   return (
     <section className="chat-container">
       <div className="chat-info">
+        {edit ? <input type="checkbox"/> : null}
         <img className="profile-pic" src={profilePpic}></img>
         <div className="chat-text-info">
           <h4 className="chat-title text">{username}</h4>
--- src/components/ContactsPanel/index.jsx
+++ src/components/ContactsPanel/index.jsx
@@ -2,14 +2,12 @@ import "./ContactsPanel.css";
 import { useCurrentView } from "../ViewManager/context/currentViewContext";
 import Contact from "../Contact";
 import useContacts from "./hooks/useContacts";
+import { useState } from "react";
 
 export default function ContactsPanel() {
     const { setCurrentView } = useCurrentView();
-    const {
-        contacts,
-        loading, 
-        contactsRef
-    } = useContacts();
+    const { contacts,loading, contactsRef } = useContacts();
+    const [edit, setEdit] = useState(true);
 
     return (
         <div className="contacts-panel">
@@ -18,12 +16,16 @@ export default function ContactsPanel() {
                     <p className="back-btn" onClick={(e) => setCurrentView("home")}>←</p>
                     <h2 className="app-title">Contacts</h2>
                 </div>
+                <div className="display-flex">
+                    {edit ? <p className="delete-btn small-info-text">Cancel</p> : null}
+                    {edit ? <p className="delete-btn small-info-text">Delete</p> : null}
+                </div>
             </header>
             <p className="actions border-bottom">New Chat</p>
             <p onClick={(e) => setCurrentView("add-contact")} className="actions border-bottom">New contact</p>
             <p className="subtitle">Contacts in Loro</p>
             <div ref={contactsRef} className="contacts">
-                {contacts.map((contact, index) => (<Contact key={index} username={contact.username} />))}
+                {contacts.map((contact, index) => (<Contact key={index} edit={edit} username={contact.username} />))}
                 {loading ? <p className="small-info-text">Loading</p> : null}
             </div>
         </div>
