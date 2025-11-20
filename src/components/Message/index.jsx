5e3c775efc3ab271e6354ea0a7d286e21849bc38
calimits <calimitsacd@gmail.com>
2025-11-20 16:37:00 -0500

    working on sending messages and deleting msgc

--- src/components/Message/index.jsx
+++ src/components/Message/index.jsx
@@ -4,13 +4,13 @@ import "./Message.css"
 export default function Message({classNames, message, deleteMsg, setDeleteMsg}) {
     const d = new Date(message.dateTime); 
     const [date, setDate] = useState(`${d.getMonth()}/${d.getDate()}-${d.getHours()}:${d.getMinutes()}`)
-    
+
     return(
         <div className={`${classNames} message`}>
             <p className="text-message">{message.content}</p>
             <div className="to-the-right flex-container">
                 {!message.unSent ? <p className="text-message small-info-text">✔</p> : null}
-                {message.messageVerificationStatus.length === 1 && message.statusVerification.isRecieved ? <p className="small-info-text">✔</p> : null}
+                {message.messageVerificationStatus.length === 1 && message.messageVerificationStatus.isRecieved ? <p className="small-info-text">✔</p> : null}
                 <p className="text-message small-info-text">{date}</p>
                 {deleteMsg ? <input className="margin-rem" type="checkbox"/> : null}
             </div>
